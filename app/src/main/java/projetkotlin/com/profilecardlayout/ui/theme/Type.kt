b4a3b95b8d674a4f8cffcac3bcc592952e9d2472
unidesk <vinicius.nogueira@unidesk.com.br>
2022-12-02 08:39:47 -0300

    Estilização de fontes de tipografia

--- app/src/main/java/projetkotlin/com/profilecardlayout/ui/theme/Type.kt
+++ app/src/main/java/projetkotlin/com/profilecardlayout/ui/theme/Type.kt
@@ -12,7 +12,12 @@ val Typography = Typography(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
+    ),
+    h5 = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+        letterSpacing = 1.sp
+    ),
     /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
