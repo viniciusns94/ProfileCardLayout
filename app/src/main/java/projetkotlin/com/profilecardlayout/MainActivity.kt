12e5bee7921453db5ada3b4d9134604923ff879c
unidesk <vinicius.nogueira@unidesk.com.br>
2022-12-07 17:21:59 -0300

    LazyColumn implementation

--- app/src/main/java/projetkotlin/com/profilecardlayout/MainActivity.kt
+++ app/src/main/java/projetkotlin/com/profilecardlayout/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -44,13 +46,10 @@ fun MainScreen(userProfiles: List<UserProfile> = userProfileList) {
         Surface(
             modifier = Modifier.fillMaxSize(),
         ) {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.SpaceEvenly,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                for (userProfile in userProfiles)
+            LazyColumn {
+                items(userProfiles) { userProfile ->
                     ProfileCard(userProfile = userProfile)
+                }
             }
         }
     }
--- app/src/main/java/projetkotlin/com/profilecardlayout/UserProfile.kt
+++ app/src/main/java/projetkotlin/com/profilecardlayout/UserProfile.kt
@@ -4,5 +4,21 @@ data class UserProfile(val name: String, val status: Boolean, val drawableId: In
 
 val userProfileList = arrayListOf(
     UserProfile(name = "Dolores Freitas", status = true, drawableId = R.drawable.profile_picture),
-    UserProfile(name = "Ana Joans", status = false, drawableId = R.drawable.profile_picture2)
+    UserProfile(name = "Ana Joans", status = false, drawableId = R.drawable.profile_picture2),
+    UserProfile(name = "Dolores Freitas", status = true, drawableId = R.drawable.profile_picture),
+    UserProfile(name = "Ana Joans", status = false, drawableId = R.drawable.profile_picture2),
+    UserProfile(name = "Dolores Freitas", status = true, drawableId = R.drawable.profile_picture),
+    UserProfile(name = "Ana Joans", status = false, drawableId = R.drawable.profile_picture2),
+    UserProfile(name = "Dolores Freitas", status = true, drawableId = R.drawable.profile_picture),
+    UserProfile(name = "Ana Joans", status = false, drawableId = R.drawable.profile_picture2),
+    UserProfile(name = "Dolores Freitas", status = true, drawableId = R.drawable.profile_picture),
+    UserProfile(name = "Ana Joans", status = false, drawableId = R.drawable.profile_picture2),
+    UserProfile(name = "Dolores Freitas", status = true, drawableId = R.drawable.profile_picture),
+    UserProfile(name = "Ana Joans", status = false, drawableId = R.drawable.profile_picture2),
+    UserProfile(name = "Dolores Freitas", status = true, drawableId = R.drawable.profile_picture),
+    UserProfile(name = "Ana Joans", status = false, drawableId = R.drawable.profile_picture2),
+    UserProfile(name = "Dolores Freitas", status = true, drawableId = R.drawable.profile_picture),
+    UserProfile(name = "Ana Joans", status = false, drawableId = R.drawable.profile_picture2),
+    UserProfile(name = "Dolores Freitas", status = true, drawableId = R.drawable.profile_picture),
+    UserProfile(name = "Ana Joans", status = false, drawableId = R.drawable.profile_picture2),
 )
